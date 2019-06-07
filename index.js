9e2c73bf6b96474b38cb131de1b9b06a531063aa
l2sants <doslixo@gmail.com>
2019-06-07 05:56:05 -0300

    iniciando

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended:true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, err => {
+    if(err) {
+        console.log('algo aconteceu')
+    }
+    else {
+        console.log('convert my money ON!!')
+    }
+    
+})
