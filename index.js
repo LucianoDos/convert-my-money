d1ebba7963cee0d4e386e87240283fb2b77ccb73
l2sants <doslixo@gmail.com>
2019-06-10 21:54:02 -0300

    adiconando: form, view cotacao

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const convert = require('./lib/convert')
 
 const port = process.env.PORT || 3000
 
@@ -12,6 +13,23 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if(cotacao && quantidade) {
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            error: false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        })
+    }else {
+        res.render('cotacao', {
+            error: 'Valores invalidos'
+        })
+    }
 })
 
 app.listen(port, err => {
